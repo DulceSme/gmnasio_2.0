33018a95be28bfc12193c31ae6f1ed946bb2f00a
dulce <210542@utxicotepec.edu.mx>
2024-07-04 10:41:22 -0600

    correcion de personas

--- Backend/models/persons.py
+++ Backend/models/persons.py
@@ -3,13 +3,19 @@ from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
 from config.db import Base
 
-class User(Base):
+class Person(Base):
     __tablename__ = "persons"
 
     id = Column(Integer, primary_key=True, index=True)
     Titulo_Cortesia = Column(String(255))
-    password = Column(LONGTEXT)
-    created_at = Column(DateTime)
-    estatus = Column(Boolean, default=False)
-    Id_persona = Column(Integer)
-    #items = relationship("Item", back_populates="owner") Clave Foranea
+    Nombre = Column(String(255))
+    Primer_Apellido = Column(String(255))
+    Segundo_Apellido = Column(String(255))
+    Fecha_Nacimiento = Column(DateTime)
+    Fotografia = Column(LONGTEXT)
+    Genero = Column(String(255))
+    Tipo_Sangre = Column(String(255))
+    Estatus = Column(Boolean, default=False)
+    Fecha_Registro = Column(DateTime)
+    Fecha_Actualizacion = Column(DateTime)
+    #items = relationship("Item", back_populates="owner")
