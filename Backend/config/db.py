550a1c606f176cce58debb779ccc71ac4742185f
dulce <210542@utxicotepec.edu.mx>
2024-07-09 10:50:06 -0600

    conexion con aiven

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_w5V21kW9pfSV8Ypbo7a@mysql-899b561-utxicotepec-9d53.l.aivencloud.com:19027/defaultdb"
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
