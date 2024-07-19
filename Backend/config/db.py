d4ef69e7408aeba4de2ec6d78e58c523ee05d7ff
dulce <210542@utxicotepec.edu.mx>
2024-07-19 12:26:25 -0600

    url

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql://avnadmin:AVNS_w5V21kW9pfSV8Ypbo7a@mysql-899b561-utxicotepec-9d53.l.aivencloud.com:19027/defaultdb?ssl-mode=REQUIRED'
+SQLALCHEMY_DATABASE_URL = 'mysql://avnadmin:AVNS_w5V21kW9pfSV8Ypbo7a@mysql-899b561-utxicotepec-9d53.l.aivencloud.com:19027/defaultdb'
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@localhost:3307/test'  #Conexión local
 
 #  Conexión local
