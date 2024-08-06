b290891ca0af7b223ff2d9b7fc980bf274343169
dulce <210542@utxicotepec.edu.mx>
2024-08-06 13:13:18 -0600

    opinion cliente

--- Backend/app.py
+++ Backend/app.py
@@ -4,6 +4,7 @@ from routes.person import person
 from routes.rol import rol
 from routes.usersrols import userrol
 from routes.Pregunta import pregunta_router 
+from routes.OpinionCliente import opinion_router
 
 app = FastAPI()
 app.include_router(user)
@@ -11,4 +12,5 @@ app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
 app.include_router(pregunta_router )
+app.include_router(opinion_router)
 print("Hola bienvenido a mi backend")
