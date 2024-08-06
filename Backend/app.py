88a34b0011804f3317f4b0cd9a559eb98adb019b
dulce <210542@utxicotepec.edu.mx>
2024-08-06 10:45:13 -0600

    bd preguntas

--- Backend/app.py
+++ Backend/app.py
@@ -3,10 +3,12 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.usersrols import userrol
+from routes.Pregunta import pregunta_router 
 
 app = FastAPI()
 app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
+app.include_router(pregunta_router )
 print("Hola bienvenido a mi backend")
